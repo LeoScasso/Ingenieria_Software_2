3503a5ecf9946dedbfb38dd30359df5bc05c57e1
Gonzalo Ramiro Irigoyen <irigoyengon@hotmail.com>
2025-05-15 19:02:26 -0300

    Subo la base de datos y creo la conexion

--- backend/app.py
+++ backend/app.py
@@ -4,9 +4,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/api/hello')
-def hello():
-    return 'Hello world!'
+app.secret_key = 'grupo57'
 
 if __name__ == '__main__':
     app.run(debug=True) 
