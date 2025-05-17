d256beb1a25caf05b5cd78472cfe58ae7048e62a
Agustin Natalio <agus.natalio2305@gmail.com>
2025-05-17 19:02:30 -0300

    Modificaciones correspondientes en AppRouter para que se visualice el Home, navbar y footer correctamente

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './components/auth/Login'; // Assuming Login component path
+import BackgroundLogo from './components/bgLogo/bgLogo';
+import Navbar from './components/navbar/navbar';
+import Home from './components/Home/Home'; // Assuming Home component path
+import Login from './components/auth/Login'; // Assuming Login component path}
+import Footer from './components/footer/footer';
 
 const AppRouter = () => {
   return (
     <Router>
+      <BackgroundLogo />
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
       </Routes>
+      <Footer />
     </Router>
   );
 };
