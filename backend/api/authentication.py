f6fbf7d99be3a88b5866acb56ed3e82fe027bddc
LeoScasso <LeanScasso@outlook.com>
2025-05-20 15:51:26 -0300

    Reforma al authentication.py

--- backend/api/authentication.py
+++ backend/api/authentication.py
@@ -15,43 +15,25 @@ def login():
     mail = data.get('mail')
     password = data.get('password')
 
+    user_sources = [
+        (users, 'user', 'user_id'),
+        (employees, 'employee', 'employee_id'),
+        (admins, 'admin','admin_id')
+    ]
     with engine.connect() as conn:
-        # Busca en users
-        stmt = select(users).where(users.c.mail == mail)
-        result = conn.execute(stmt).fetchone()
-        if result and result.password == password:
-            session['user_id'] = result.user_id
-            session['user_role'] = 'user'
-            return jsonify({
-                'message': 'Sesión iniciada correctamente',
-                'id': session['user_id'],
-                'role': session['role']
-            })
-        
-        # Busca en employees
-        stmt = select(employees).where(employees.c.mail == mail)
-        result = conn.execute(stmt).fetchone()
-        if result and result.password == password:
-            session['user_id'] = result.employee_id
-            session['user_role'] = 'employee'
-            return jsonify({
-                'message': 'Sesión iniciada correctamente',
-                'id': session['user_id'],
-                'role': session['role']
-            })
-        
-        # Busca en admins
-        stmt = select(admins).where(admins.c.mail == mail)
-        result = conn.execute(stmt).fetchone()
-        if result and result.password == password:
-            session['user_id'] = result.admin_id
-            session['user_role'] = 'admin'
-            return jsonify({
-                'message': 'Sesión iniciada correctamente',
-                'id': session['user_id'],
-                'role': session['role']
-            })
-        return {'error': 'Email o contraseña incorrectos'}
+        for table, role, id_field in user_sources:
+            stmt = select(table).where(table.c.mail == mail)
+            result = conn.execute(stmt).fetchone()
+            if result and result.password == password:
+                session['user_id'] = getattr(result,id_field)
+                session['user_role'] = role
+                return jsonify({
+                    'message': 'Sesión iniciada correctamente',
+                    'user_id': session['user_id'],
+                    'user_role': session['user_role']
+                })
+    
+    return {'error': 'Email o contraseña incorrectos'}
 
 
 @auth_bp.route('/logout', methods=['POST'])
