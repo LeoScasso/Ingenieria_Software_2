2ac8c9caa2635a8c803f28de1a2378e0354eb7a9
LeoScasso <LeanScasso@outlook.com>
2025-05-21 20:28:20 -0300

    actualizacion db.py

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,4 +1,4 @@
 from sqlalchemy import MetaData, create_engine
 
-engine = create_engine('sqlite:///../data/DataBase v1.1.db', echo = True)
+engine = create_engine('sqlite:///../data/DataBase v1.2.db', echo = True)
 metadata = MetaData()
