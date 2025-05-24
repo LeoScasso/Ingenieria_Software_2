55edf6bf395fddb89fa4787d514b364fbdc414f1
Agustin Natalio <agus.natalio2305@gmail.com>
2025-05-24 20:21:17 -0300

    Implementada lógica en el navbar para ocultar botones según vista actual

--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -1,7 +1,11 @@
 import { AppBar, Toolbar, Button, Box } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const Navbar = () => {
+  const location = useLocation();
+  const hideRegisterButton = location.pathname === '/register';
+  const hideLoginButton = location.pathname === '/login';
+
   return (
     <AppBar position="static" sx={{ backgroundColor: 'darkBlue' }}>
       <Toolbar sx={{ justifyContent: 'space-between' }}>
@@ -12,23 +16,27 @@ const Navbar = () => {
 
         {/* Botones */}
         <Box>
-          <Button
-            component={Link}
-            to="/login"
-            variant="contained"
-            color="primary"
-            sx={{ backgroundColor: 'beanBlue', marginRight: 1 }}
-          >
-            Iniciar sesión
-          </Button>
-          <Button
-            component={Link}
-            to="/register"
-            variant="outlined"
-            sx={{ borderColor: 'beige', color: 'beige' }}
-          >
-            Registrarse
-          </Button>
+          {!hideLoginButton && (
+            <Button
+              component={Link}
+              to="/login"
+              variant="contained"
+              color="primary"
+              sx={{ backgroundColor: 'beanBlue', marginRight: 1 }}
+            >
+              Iniciar sesión
+            </Button>
+          )}
+          {!hideRegisterButton && (
+            <Button
+              component={Link}
+              to="/register"
+              variant="outlined"
+              sx={{ borderColor: 'beige', color: 'beige' }}
+            >
+              Registrarse
+            </Button>
+          )}
         </Box>
       </Toolbar>
     </AppBar>
