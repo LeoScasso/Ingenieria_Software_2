7506ee94ac08f0e7a462efef740626f7db5a5cff
Gonzalo Ramiro Irigoyen <irigoyengon@hotmail.com>
2025-05-29 18:37:41 -0300

    refactorizacion del codigo debido a la modificacion de la categoria

--- backend/app/db.py
+++ backend/app/db.py
@@ -2,7 +2,7 @@ import os
 from sqlalchemy import MetaData, create_engine
 
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
-DB_PATH = os.path.join(BASE_DIR, 'data', 'DataBase v1.2.db')
+DB_PATH = os.path.join(BASE_DIR, 'data', 'DataBase v1.3.db')
 
 engine = create_engine(f'sqlite:///{DB_PATH}', echo=True)
 metadata = MetaData()
