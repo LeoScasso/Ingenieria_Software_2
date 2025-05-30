b3ce9d4ac63eedd6a98716e7d97baa481e8a3199
Lean <canosa.leandro@gmail.com>
2025-05-30 12:59:03 -0300

    SQLAlchemy logging supress INFO

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,8 +1,15 @@
 import os
+import logging
 from sqlalchemy import MetaData, create_engine
+
+# Configurar el logging de SQLAlchemy
+logging.getLogger('sqlalchemy.engine').setLevel(logging.WARNING)
+logging.getLogger('sqlalchemy.dialects').setLevel(logging.WARNING)
+logging.getLogger('sqlalchemy.pool').setLevel(logging.WARNING)
+logging.getLogger('sqlalchemy.orm').setLevel(logging.WARNING)
 
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
 DB_PATH = os.path.join(BASE_DIR, 'data', 'DataBase v1.3.db')
 
-engine = create_engine(f'sqlite:///{DB_PATH}', echo=True)
+engine = create_engine(f'sqlite:///{DB_PATH}', echo=False)
 metadata = MetaData()
