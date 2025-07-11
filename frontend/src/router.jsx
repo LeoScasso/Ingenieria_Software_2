648be86d68928d3ff47f7830d30a67b118af5012
Agustin Natalio <agus.natalio2305@gmail.com>
2025-07-10 21:58:22 -0300

    Adición de ruta de historial de reservas que ve el empleado

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -20,6 +20,7 @@ import ReservationForm from './components/reservation/ReservationForm'
 import { EditProfile } from './components/user/EditProfile'
 import { Profile } from './components/user/Profile'
 import RentalHistory from './components/user/RentalHistory'
+import RentalHistoriEmp from './components/employee/RentalHistoryEmp'
 import { theme } from './theme/theme'
 
 const AppRouter = () => {
@@ -62,6 +63,7 @@ const AppRouter = () => {
             <Route path="/branches" element={<BranchesList />} />
             <Route path="/add-packages/:id" element={<AddPackages />} />
             <Route path="/employee-registration" element={<EmployeeRegForm />} />
+            <Route path="/rental-history-emp" element={<RentalHistoriEmp />} />
           </Routes>
         </Box>
         <Footer />
